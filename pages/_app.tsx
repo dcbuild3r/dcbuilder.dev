e62e4bd46dd8f99cdba9dc8b33fc01bf33c9dd5a
dcbuilder <dcbuilder@protonmail.com>
2022-03-14 00:04:38 +0100

    fix(metadata images)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,29 @@ function MyApp({ Component, pageProps }: AppProps) {
 					property="og:image"
 					content="https://raw.githubusercontent.com/dcbuild3r/dcbuilder.dev/master/public/images/dcbuilder.webp"
 				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta property="twitter:domain" content="dcbuilder.dev" />
+				<meta property="twitter:url" content="https://dcbuilder.dev/" />
+				<meta name="twitter:title" content="dcbuilder" />
+				<meta
+					name="twitter:description"
+					content="dcbuilder.eth - full-stack blockchain developer and researcher engineer at Alongside Finance. Ethereum, devpill.me, TribeDAO, Waifus Anonymous."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://raw.githubusercontent.com/dcbuild3r/dcbuilder.dev/master/public/images/dcbuilder.webp"
+				/>
+				<meta property="og:url" content="https://dcbuilder.dev/" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="dcbuilder" />
+				<meta
+					property="og:description"
+					content="dcbuilder.eth - full-stack blockchain developer and researcher engineer at Alongside Finance. Ethereum, devpill.me, TribeDAO, Waifus Anonymous."
+				/>
+				<meta
+					property="og:image"
+					content="https://raw.githubusercontent.com/dcbuild3r/dcbuilder.dev/master/public/images/dcbuilder.webp"
+				/>
 				<link rel="shortcut icon" href="favicon.ico" />
 			</Head>
 			<DefaultSeo {...SEO} />
