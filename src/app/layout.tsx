5794fddbdf963613c2550404bb242e51f47e3ad5
dcbuilder.eth <dcbuilder@protonmail.com>
2026-01-27 21:50:46 +0100

    fix: use app directory favicon convention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<head>
-				<link rel="icon" href="/favicon.ico" />
-			</head>
 			<body className="antialiased">
 				<ThemeProvider>{children}</ThemeProvider>
 			</body>
