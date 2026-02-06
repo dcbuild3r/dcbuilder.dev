cc7e795fa5525f5635fa0e777fe41556710123f2
dcbuilder.eth <dcbuilder@pm.me>
2026-02-06 13:25:17 +0100

    Backfill freshness flags in migration

--- drizzle/0004_groovy_owl.sql
+++ drizzle/0004_groovy_owl.sql
@@ -1,4 +1,10 @@
 ALTER TABLE "blog_posts" ADD COLUMN "is_fresh" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "announcements" ADD COLUMN "is_fresh" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "curated_links" ADD COLUMN "source_image" text;--> statement-breakpoint
-ALTER TABLE "curated_links" ADD COLUMN "is_fresh" boolean DEFAULT false NOT NULL;
+ALTER TABLE "curated_links" ADD COLUMN "is_fresh" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+UPDATE "blog_posts"
+SET "is_fresh" = ("date" > (timezone('UTC', now()) - interval '7 day'));--> statement-breakpoint
+UPDATE "announcements"
+SET "is_fresh" = ("date" > (timezone('UTC', now()) - interval '7 day'));--> statement-breakpoint
+UPDATE "curated_links"
+SET "is_fresh" = ("date" > (timezone('UTC', now()) - interval '7 day'));
